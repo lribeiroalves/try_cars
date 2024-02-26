d428e8a9efb5180ddd3b705757f9a2c648016899
Lucas Ribeiro <145038823+lribeiroalves@users.noreply.github.com>
2024-02-26 13:16:28 -0300

    Update app.py

--- trycars/app.py
+++ trycars/app.py
@@ -1,13 +1,6 @@
 from flask import Flask
 
 from trycars.ext.configuration import configuration
-# from trycars.ext.database import database
-# from trycars.ext.commands import commands
-# from trycars.ext.mail_client import mail_client
-
-# from trycars.blueprints.development import bp_dev
-# from trycars.blueprints.homepage import bp_home
-# from trycars.blueprints.auth import bp_auth
 
 
 def minimal_app(**config):
@@ -20,12 +13,5 @@ def minimal_app(**config):
 def create_app(**config):
     app = minimal_app(**config)
     configuration.load_extensions(app)
-    # database.init_app(app)
-    # commands.init_app(app)
-    # mail_client.init_app(app)
-
-    # bp_dev.init_app(app)
-    # bp_home.init_app(app)
-    # bp_auth.init_app(app)
 
     return app
